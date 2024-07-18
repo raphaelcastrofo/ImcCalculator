94bb223e0bddb0d09857b5e77cf0e578dea9cda2
raphaelcastrofo <raphaelcastrofo@gmail.com>
2024-07-18 15:30:06 +0200

    Inicial scream editText

--- app/src/main/java/com/comunidadedevspace/imc/MainActivity.kt
+++ app/src/main/java/com/comunidadedevspace/imc/MainActivity.kt
@@ -2,10 +2,26 @@ package com.comunidadedevspace.imc
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import com.google.android.material.textfield.TextInputEditText
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val edtWeight = findViewById<TextInputEditText>(R.id.edt_weight)
+        val edtHeight = findViewById<TextInputEditText>(R.id.edt_height)
+
+        val btnCalculate = findViewById<Button>(R.id.btn_Calculate)
+
+        btnCalculate.setOnClickListener {
+            val weight = edtWeight.text
+            val height = edtHeight.text
+
+            println("o peso eh: " + weight + " e a altura eh: " + height)
+
+        }
+
     }
 }
